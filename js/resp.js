ff0aac46ae2a77275d47a8af92c7067161b1c078
SHUBHAM <wurtzreaxnwittigreaxn@gmail.com>
2021-06-01 00:11:22 +0530

    easyWays

---
+++ js/resp.js
@@ -0,0 +1,13 @@
+
+function myFunction() {
+    var x = document.querySelector(".navbar")
+    var y = document.querySelector(".rightNav")
+    var z = document.querySelector(".nav-list")
+    var w = document.querySelector(".burger")
+    x.classList.toggle('h-nav-resp');
+    y.classList.toggle('v-class-resp');
+    z.classList.toggle('v-class-resp');
+    w.classList.toggle('.height-correction')
+    
+  }
+  
