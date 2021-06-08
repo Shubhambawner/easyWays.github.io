c3b9d5cf603443bf93956fdbe89c663e25aa91b8
SHUBHAM <wurtzreaxnwittigreaxn@gmail.com>
2021-06-08 12:15:55 +0530

    mainSite

---
+++ mainSite/perticulars.js
@@ -0,0 +1,20 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  function myFunctiont(e) {
+    var e1 = document.getElementById(e);
+    var x = document.querySelector(".active");
+    
+    x.classList.toggle('active');
+    e1.classList.toggle('active');
+    
+  }
